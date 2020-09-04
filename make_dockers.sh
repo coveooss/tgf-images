16a9e816114c83549805939db5e677ee71ffa169
Julien Duchesne <jduchesne@coveo.com>
2020-09-04 10:45:48 -0400

    Update Terragrunt Move to Github Actions

--- make_dockers.sh
+++ make_dockers.sh
@@ -1,10 +1,10 @@
 set -e
 
-# If TRAVIS_TAG is not set, we recuperate the last tag for the repo
-: ${TRAVIS_TAG:=$(git describe --abbrev=0 --tags)}
+# If GIT_TAG is not set, we recuperate the last tag for the repo
+: ${GIT_TAG:=$(git describe --abbrev=0 --tags)}
 
 # If the tag does not start with image- we ignore it
-[ "${TRAVIS_TAG::1}" != v ] && exit 0
+[ "${GIT_TAG::1}" != v ] && exit 0
 
 for df in Dockerfile*
 do
@@ -14,20 +14,20 @@ do
     tag=${df:13}
     tag=${tag,,}
     [ -z ${tag} ] || tag=-${tag}
-    travis_tag=${TRAVIS_TAG:1}
-    travis_maj_min=${travis_tag%.*}
-    version=coveo/tgf:${travis_tag}${tag}
-    version_mm=coveo/tgf:${travis_maj_min}${tag}
+    git_tag=${GIT_TAG:1}
+    git_maj_min=${git_tag%.*}
+    version=ghcr.io/coveooss/tgf:${git_tag}${tag}
+    version_mm=ghcr.io/coveooss/tgf:${git_maj_min}${tag}
     latest=${tag:1}
-    latest=coveo/tgf:${latest:-latest}
+    latest=ghcr.io/coveooss/tgf:${latest:-latest}
 
     # We do not want to tag latest if this is not an official version number
-    [[ $travis_tag == *-* ]] && unset latest
+    [[ $git_tag == *-* ]] && unset latest
     
     dockerfile=dockerfile.temp
-    # We replace the TRAVIS_TAG variable if any (case where the image is build from another image)
+    # We replace the GIT_TAG variable if any (case where the image is build from another image)
     # The result file is simply named Dockerfile
-    cat $df | sed -e "s/\${TRAVIS_TAG}/$travis_tag/" | sed -e "s/\TGF_IMAGE_MAJ_MIN=/TGF_IMAGE_MAJ_MIN=$travis_maj_min/" > $dockerfile
+    cat $df | sed -e "s/\${GIT_TAG}/$git_tag/" | sed -e "s/\TGF_IMAGE_MAJ_MIN=/TGF_IMAGE_MAJ_MIN=$git_maj_min/" > $dockerfile
 
     docker build -f $dockerfile -t $version . && rm $dockerfile
     docker push $version

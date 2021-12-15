90d98b6f0fedc732630b79de6bfdc8e538ce9424
Boris Bera <bbera@coveo.com>
2021-12-15 11:22:37 -0500

    Reformat install scripts to be easier on the eyes

--- Dockerfile.0_scripts/download_executables.sh
+++ Dockerfile.0_scripts/download_executables.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-
 set -xeu
 
-if [ "$TARGETPLATFORM" = linux/arm64 ]
-then
+if [ "$TARGETPLATFORM" = linux/arm64 ]; then
   TF_PLATFORM=linux_arm64
   EXT=linux_arm64
 else
@@ -15,20 +13,20 @@ fi
 TERRAFORM_PATH=https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${TF_PLATFORM}.zip
 echo "Downloading terraform from $TERRAFORM_PATH"
 curl -sLo_ "${TERRAFORM_PATH}"
-unzip -p _ > "${EXE_FOLDER}"/terraform
-chmod +x "${EXE_FOLDER}"/terraform
+unzip -p _ > "${EXE_FOLDER}/terraform"
+chmod +x "${EXE_FOLDER}/terraform"
 rm -f _
 
 TERRAGRUNT_PATH=https://github.com/coveo/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_${TERRAGRUNT_VERSION}_${EXT}.zip
 echo "Downloading terragrunt from $TERRAGRUNT_PATH"
 curl -sLo_ "${TERRAGRUNT_PATH}"
-unzip -p _ > "${EXE_FOLDER}"/terragrunt
-chmod +x "${EXE_FOLDER}"/terragrunt
+unzip -p _ > "${EXE_FOLDER}/terragrunt"
+chmod +x "${EXE_FOLDER}/terragrunt"
 rm -f _
 
 GOTEMPLATE_PATH=https://github.com/coveo/gotemplate/releases/download/v${GOTEMPLATE_VERSION}/gotemplate_${GOTEMPLATE_VERSION}_${EXT}.zip
 echo "Downloading gotemplate from $GOTEMPLATE_PATH"
 curl -sLo_ "${GOTEMPLATE_PATH}"
-unzip -p _ > "${EXE_FOLDER}"/gotemplate
-chmod +x "${EXE_FOLDER}"/gotemplate
+unzip -p _ > "${EXE_FOLDER}/gotemplate"
+chmod +x "${EXE_FOLDER}/gotemplate"
 rm -f _
--- Dockerfile.1_scripts/download_executables.sh
+++ Dockerfile.1_scripts/download_executables.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-
 set -xeu
 
-if [ "$TARGETPLATFORM" = linux/arm64 ]
-then
+if [ "$TARGETPLATFORM" = linux/arm64 ]; then
   PLATFORM=arm64
   TF_DOCS_PLATFORM=arm64
 else
@@ -14,11 +12,11 @@ fi
 TFLINT_PATH=https://github.com/terraform-linters/tflint/releases/download/v${TF_LINT_VERSION}/tflint_linux_${PLATFORM}.zip
 echo "Downloading tflint from $TFLINT_PATH"
 curl -sLo_ "${TFLINT_PATH}"
-unzip -p _ > "${EXE_FOLDER}"/tflint
-chmod +x "${EXE_FOLDER}"/tflint
+unzip -p _ > "${EXE_FOLDER}/tflint"
+chmod +x "${EXE_FOLDER}/tflint"
 rm _
 
 TERRAFORM_DOCS_PATH=https://github.com/coveord/terraform-docs/releases/download/v${TF_DOC_VERSION}/terraform-docs-v${TF_DOC_VERSION}-linux-${TF_DOCS_PLATFORM}
 echo "Downloading terraform-docs from $TERRAFORM_DOCS_PATH"
-curl -sLo "${EXE_FOLDER}"/terraform-docs "${TERRAFORM_DOCS_PATH}"
-chmod +x "${EXE_FOLDER}"/terraform-docs
+curl -sLo "${EXE_FOLDER}/terraform-docs" "${TERRAFORM_DOCS_PATH}"
+chmod +x "${EXE_FOLDER}/terraform-docs"

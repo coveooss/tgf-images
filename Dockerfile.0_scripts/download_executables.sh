e6871d96a113cd5fa9028b0adbb1d73d74f1be32
Boris Bera <bbera@coveo.com>
2021-12-15 11:17:00 -0500

    Autofix shellcheck issues

--- Dockerfile.0_scripts/download_executables.sh
+++ Dockerfile.0_scripts/download_executables.sh
@@ -14,21 +14,21 @@ fi
 
 TERRAFORM_PATH=https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${TF_PLATFORM}.zip
 echo "Downloading terraform from $TERRAFORM_PATH"
-curl -sLo_ ${TERRAFORM_PATH}
-unzip -p _ > ${EXE_FOLDER}/terraform
-chmod +x ${EXE_FOLDER}/terraform
+curl -sLo_ "${TERRAFORM_PATH}"
+unzip -p _ > "${EXE_FOLDER}"/terraform
+chmod +x "${EXE_FOLDER}"/terraform
 rm -f _
 
 TERRAGRUNT_PATH=https://github.com/coveo/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_${TERRAGRUNT_VERSION}_${EXT}.zip
 echo "Downloading terragrunt from $TERRAGRUNT_PATH"
-curl -sLo_ ${TERRAGRUNT_PATH}
-unzip -p _ > ${EXE_FOLDER}/terragrunt
-chmod +x ${EXE_FOLDER}/terragrunt
+curl -sLo_ "${TERRAGRUNT_PATH}"
+unzip -p _ > "${EXE_FOLDER}"/terragrunt
+chmod +x "${EXE_FOLDER}"/terragrunt
 rm -f _
 
 GOTEMPLATE_PATH=https://github.com/coveo/gotemplate/releases/download/v${GOTEMPLATE_VERSION}/gotemplate_${GOTEMPLATE_VERSION}_${EXT}.zip
 echo "Downloading gotemplate from $GOTEMPLATE_PATH"
-curl -sLo_ ${GOTEMPLATE_PATH}
-unzip -p _ > ${EXE_FOLDER}/gotemplate
-chmod +x ${EXE_FOLDER}/gotemplate
+curl -sLo_ "${GOTEMPLATE_PATH}"
+unzip -p _ > "${EXE_FOLDER}"/gotemplate
+chmod +x "${EXE_FOLDER}"/gotemplate
 rm -f _
--- Dockerfile.2_scripts/download_executables.sh
+++ Dockerfile.2_scripts/download_executables.sh
@@ -10,15 +10,15 @@ else
 fi
 
 # Install kubectl
-curl -sLo_ https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/${TARGETPLATFORM}/kubectl && \
-    mv _ ${EXE_FOLDER}/kubectl && \
-    chmod +x ${EXE_FOLDER}/kubectl && \
+curl -sLo_ https://storage.googleapis.com/kubernetes-release/release/v"${KUBE_VERSION}"/bin/"${TARGETPLATFORM}"/kubectl && \
+    mv _ "${EXE_FOLDER}"/kubectl && \
+    chmod +x "${EXE_FOLDER}"/kubectl && \
     mkdir -p /home/tgf/.kube && \
-    touch ${KUBECONFIG} && \
-    chmod 666 ${KUBECONFIG}
+    touch "${KUBECONFIG}" && \
+    chmod 666 "${KUBECONFIG}"
 
 # Install helm (git is also required by helm)
-curl -sL https://get.helm.sh/helm-v${HELM_VERSION}-${HELM_PLATFORM}.tar.gz | tar xzo && \
-    mv ./${HELM_PLATFORM}/helm ${EXE_FOLDER}/helm && \
+curl -sL https://get.helm.sh/helm-v"${HELM_VERSION}"-${HELM_PLATFORM}.tar.gz | tar xzo && \
+    mv ./${HELM_PLATFORM}/helm "${EXE_FOLDER}"/helm && \
     rm -rf ./${HELM_PLATFORM} && \
-    chmod +x $EXE_FOLDER/helm
+    chmod +x "$EXE_FOLDER"/helm
